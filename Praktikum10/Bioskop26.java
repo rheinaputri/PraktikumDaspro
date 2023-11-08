c58173bf2b48d33132f379af7d45ed3e1e9dfc9c
rheinaputri <rheinaputri8@gmail.com>
2023-11-08 14:20:18 +0700

    ketiga

--- Praktikum10/Bioskop26.java
+++ Praktikum10/Bioskop26.java
@@ -26,10 +26,18 @@ public class Bioskop26{
         // System.out.println(penonton[2].length);
         // System.out.println(penonton[3].length);
 
-        System.out.println (penonton.length);
-        for (int i = 0; i < penonton.length; i++){
-            System.out.println("Panjang baris ke-" + (i+1) + ":" + penonton[i].length);
+        // System.out.println (penonton.length);
+        // for (int i = 0; i < penonton.length; i++){
+        //     System.out.println("Panjang baris ke-" + (i+1) + ":" + penonton[i].length);
 
+//         System.out.println(penonton.length);
+//         for (String[] barisPenonton : penonton) {
+//             System.out.println("Panjang baris: " + barisPenonton.length);
+
+
+        System.out.println("Penonton pada baris ke-3:");
+        for (int i = 0; i < penonton[2].length; i++){
+            System.out.println(penonton[2][1]);
 }
         }
     }
