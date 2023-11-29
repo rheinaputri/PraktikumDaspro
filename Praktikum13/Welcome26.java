46d227602e8941aa6aaffd92b7ea6180a7f4f92e
rheinaputri <rheinaputri8@gmail.com>
2023-11-29 13:40:38 +0700

    origin

---
+++ Praktikum13/Welcome26.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Welcome26 {
+    public static void displayWelcomeMessage(){
+        System.out.println("Selamat datang di Politeknik Negeri Malang");
+    }
+
+    public static void main(String[] args) {
+        displayWelcomeMessage();
+    }
+}
